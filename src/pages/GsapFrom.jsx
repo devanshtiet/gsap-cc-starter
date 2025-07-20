38871dd05935216dc3932b311283875c0ea0c897
Devansh Verma <devanshverma9417623955@gmail.com>
2025-07-20 23:08:43 +0530

    initial commit

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,6 +1,20 @@
-const GsapFrom = () => {
-  // TODO: Implement the gsap.from() method
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
+
+const GsapFrom = () => {
+  // TODO: Implement the gsap.fromTo() method
+useGSAP(()=>{
+    gsap.from('#green-box',{
+      x:250,
+      repeat:-1,
+      yoyo: true,
+      rotation:360,
+      duration:2,
+      ease:'elastic'
+
+    })
+  },[]);
   return (
     <main>
       <h1>GsapFrom</h1>
